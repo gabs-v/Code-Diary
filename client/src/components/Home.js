42e2301efcf2cde7ec1600827e5dcc6e4ca78f39
gabs-v <gabyvazquez8297@gmail.com>
2022-08-16 19:06:21 -0700

    front

---
+++ client/src/components/Home.js
@@ -0,0 +1,11 @@
+import {Link, useNavigate} from "react-router-dom";
+
+const Home = (props) => {
+    return(
+        <div>
+            <h1>Code Diary</h1>
+        </div>
+    )
+}
+
+export default Home;
